2da795d99ecc8b7291647cebe794aec9007e6e15
doctormemes <ohwowitsdoc@protonmail.com>
2021-01-07 08:18:19 -0600

    Took out vscode directories, the size of the extensions and everything is too much to store on this repo

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@ do
  ln -srfv $i $HOME/
 done
 
-ln -srfv .p10k.zsh $HOME/
 ln -srfv .bashrc $HOME/
+ln -srfv .dmrc $HOME/
+ln -srfv .p10k.zsh $HOME/
 ln -srfv .profile $HOME/
+ln -srfv .themes $HOME/
+ln -srfv .vim $HOME
 ln -srfv .vimrc $HOME/
