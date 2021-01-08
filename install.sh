c4b84e5b5605e40a0635a50e8d06708ce248332a
doctormemes <ohwowitsdoc@protonmail.com>
2021-01-07 19:31:30 -0600

    Removed .vim folder since most people won't be using kite. Also modified conda_setup replacing my home directory path (/home/doc) with $HOME so that it'll work for anyone with the conda_setup, as long as it's sourced in .zshrc as well.

--- install.sh
+++ install.sh
@@ -29,8 +29,9 @@ else
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     echo -e "Starting Miniconda3 setup"
     bash ~/.Miniconda3-latest-Linux-x86_64.sh
-    conda init zsh
 fi
+
+
 
 
 # OMZ INSTALL
